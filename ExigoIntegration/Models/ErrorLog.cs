0c14a6629beb9e7b41b63d0926c74cb373beb4c7
makeuperaser <lexi@makeuperaser.com>
2017-04-24 16:28:10 +0530

    Add Error Log file

---
+++ ExigoIntegration/Models/ErrorLog.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace ExigoIntegration.Models
+{
+    public class ErrorLog
+    {
+        public static void LogError(Exception ex)
+        {
+            try
+            {
+                // You could use any logging approach here
+
+                StringBuilder builder = new StringBuilder();
+                builder
+                    .AppendLine("----------")
+                    .AppendLine(DateTime.Now.ToString())
+                    .AppendFormat("Source:\t{0}", ex.Source)
+                    .AppendLine()
+                    .AppendFormat("Target:\t{0}", ex.TargetSite)
+                    .AppendLine()
+                    .AppendFormat("Type:\t{0}", ex.GetType().Name)
+                    .AppendLine()
+                    .AppendFormat("Message:\t{0}", ex.Message)
+                    .AppendLine()
+                    .AppendFormat("Stack:\t{0}", ex.StackTrace)
+                    .AppendLine();
+
+                string filePath = HttpContext.Current.Server.MapPath("~/Error-" + DateTime.Now.ToString("MM-yyyy") + ".log");
+
+                using (StreamWriter writer = File.AppendText(filePath))
+                {
+                    writer.Write(builder.ToString());
+                    writer.Flush();
+                }
+            }
+            catch (Exception Ex)
+            {
+            }
+        }
+
+        public static void LogInfo(string Message)
+        {
+            try
+            {
+                // You could use any logging approach here
+
+                StringBuilder builder = new StringBuilder();
+                builder
+                    .AppendLine("----------")
+                    .AppendLine(DateTime.Now.ToString())
+                    .AppendFormat("Message:\t{0}", Message)
+                    .AppendLine();
+
+                string filePath = HttpContext.Current.Server.MapPath("~/Error-" + DateTime.Now.ToString("MM-yyyy") + ".log");
+
+                using (StreamWriter writer = File.AppendText(filePath))
+                {
+                    writer.Write(builder.ToString());
+                    writer.Flush();
+                }
+            }
+            catch (Exception Ex)
+            {
+            }
+        }
+    }
+}
